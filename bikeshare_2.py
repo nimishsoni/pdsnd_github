a979051f0f7e41460eca1a8445b4da50e3b9de33
Nimish Soni <nimish.soni@gmail.com>
2020-12-27 16:43:39 +0530

    Added comment for City, Month and Day List to bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,6 +2,10 @@ import time
 import pandas as pd
 import numpy as np
 
+"""
+List of City, Months and Days for analyzing data
+all - represents no filter 
+"""
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
